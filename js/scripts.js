329e7d83638fb1dac993031acf1bae91884683bf
Phillip Marques <phillipmarq@hotmail.com>
2023-11-29 09:49:09 -0300

    Implementing task search

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const eraseBtn = document.querySelector("#erase-button");
+const filterBtn = document.querySelector("#filter-select");
 
 let oldInputValue;
 
@@ -55,6 +58,22 @@ const updateTodo = (text) =>{
         }
     });
 };
+
+const getSearchTodos = (search) =>{
+    const todos = document.querySelectorAll(".todo")
+
+    todos.forEach((todo) =>{
+        let todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+        const normalizedSearch = search.toLowerCase();
+        todo.style.display="flex";
+
+        if(!todoTitle.includes(normalizedSearch)){
+            todo.style.display="none";
+        }
+    });
+}
+
 // Eventos
 todoForm.addEventListener("submit", (e) =>{
     e.preventDefault();
@@ -107,4 +126,17 @@ editForm.addEventListener("submit", (e)=>{
     }
 
     toggleForms()
+});
+
+searchInput.addEventListener("keyup", (e)=>{
+    const search = e.target.value;
+
+    getSearchTodos(search);
+})
+
+eraseBtn.addEventListener("click", (e)=>{
+    e.preventDefault()
+
+    searchInput.value =""
+    searchInput.dispatchEvent(new Event("keyup"))
 })
